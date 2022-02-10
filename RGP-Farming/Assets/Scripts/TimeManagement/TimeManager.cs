8361d6ab300762aaa3886f15acdaf7c8ea3f49c8
FuriousWindZ <54702765+FuriousWindZ@users.noreply.github.com>
2022-02-10 15:25:19 +0100

    Time
    Schooltijd  bezig met TimeManagement

--- RGP-Farming/Assets/Scripts/TimeManagement/TimeManager.cs
+++ RGP-Farming/Assets/Scripts/TimeManagement/TimeManager.cs
@@ -1,15 +1,18 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
 using System.Collections;
+using static Utility;
 
 public class TimeManager : MonoBehaviour
 {
 	public TextMeshProUGUI timeText;
-	private float timeValue;
-	private float hourCount;
-	private float minuteCount;
 	private string ClockAMPM = "AM";
+	private float startTime;
+
+	DateTime _startDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 8, 0, 0); DateTime currentDate;
+	DateTime Date;
 
 	[Tooltip("A Normal Day is 24 minutes")]
 	public float TimeSpeedMultiplier;
@@ -17,42 +20,59 @@ public class TimeManager : MonoBehaviour
 	public float startDayTime;
 	[Tooltip("In Minutes")]
 	public float startNightTime;
-	void Update()
-	{
-		HandleTime();
-	}
 
-	public void HandleTime()
+    private void Awake()
+	{ 
+		startTime = Time.time;
+		Debug.Log(startTime);
+    }
+    void Update()
 	{
-		//Time.Timespan elapsed time; Time.time in de project settings aanpassen.
-		// Crops kunnen gebruik maken van Time.unscaledTime zodat ze niet dezelfde waarde gebruiken als time.time [Nighel]
-		//Time multiplier kan nog aanpassen (Time.timescale past ook delta time aan. [kan ook project settings])
-		timeValue += Time.deltaTime * TimeSpeedMultiplier;
-		hourCount = (int)timeValue / 60;
-		minuteCount = (int)timeValue - (int)hourCount * 60;
-		// Full Day
-		if (timeValue >= 1440)
+		SetDate();
+	}
+	public void SetDate()
+    {
+		int timeElapsed = (int)Time.time;
+
+		int years = timeElapsed / YEAR;
+		timeElapsed %= YEAR;
+
+		int months = timeElapsed / MONTH;
+		timeElapsed %= MONTH;
+
+		int days = timeElapsed / DAY;
+		timeElapsed %= DAY;
+
+		int hours = timeElapsed / HOUR;
+		timeElapsed %= HOUR;
+
+		int minutes = timeElapsed / MINUTE;
+		timeElapsed %= MINUTE;
+
+		int seconds = timeElapsed;
+
+		DateTime currentTime = new DateTime(_startDate.Year + years, _startDate.Month + months, _startDate.Day + days, _startDate.Hour + hours, _startDate.Minute + minutes, _startDate.Second + seconds);
+
+		TimeSpan interval = currentTime - _startDate;
+
+		Debug.Log($"maanden: {interval.TotalDays / 31}, dagen: {interval.Days}, uren: {interval.Hours}, minuten: {interval.Minutes}, secondes: {interval.Seconds}");
+
+		if (hours >= 12)
 		{
-			timeValue = 0;
-			//Reset a day
-		}
-		// First half of Day
-		if (timeValue >= 720)
-		{
+			hours %= 12;
 			ClockAMPM = "PM";
-			hourCount -= 12;
 		}
-		// Second half of Day
-		else
+        else
 		{
 			ClockAMPM = "AM";
 		}
-
-		if (hourCount < 1)
-		{
-			hourCount = 12;
-		}
-		//Display in UI
-		timeText.text = hourCount.ToString("00") + ":" + minuteCount.ToString("00") + ClockAMPM;
+		timeText.text = hours.ToString("00") + ":" + minutes.ToString("00") + ClockAMPM;
+		Debug.Log(hours.ToString("00") + ":" + minutes.ToString("00") + ClockAMPM);
 	}
+	//TODO:
+	//Wanneer je savonds niet meer buiten mag zijn ga je automatisch naar bed. Dan reset de tijd naar 8AM.
+	// Dan moet hij de dag onthouden of hij er 1 bij op mag tellen. 
+	// nog gekeken worden wanneer het am en pm is.
+	// Modules is de %= en die haalt er altijd 12 af wanneer hij 12 wordt bijv.
+	
 }
