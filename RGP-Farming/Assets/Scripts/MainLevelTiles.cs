92d92f65240fbdd372d892ae7c40c0c14bcf20e6
FuriousWindZ <54702765+FuriousWindZ@users.noreply.github.com>
2022-04-15 13:56:29 +0200

    Tilemap References Fixed
    Fixed loads of references + made a mainleveltiles script to hold all the tilemaps for that specific scene

---
+++ RGP-Farming/Assets/Scripts/MainLevelTiles.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MainLevelTiles : Singleton<MainLevelTiles>
+{
+    private TilemapManager _tilemapManager => TilemapManager.Instance();
+
+    [SerializeField] public Grid MainGrid;
+
+    [SerializeField] public Tilemap HoverTilemap;
+
+    [SerializeField] public Tilemap[] TilemapsToCheck;
+
+    [SerializeField] public Tilemap[] AllTilemaps;
+
+    [SerializeField] public Tilemap[] UnwalkableTilemaps;
+    private void Awake()
+    {
+        /*_tilemapManager.MainGrid = _mainGrid;
+        _tilemapManager.HoverTilemap = _hoverTilemap;
+        _tilemapManager.TilemapsToCheck = _tilemapsToCheck;
+        _tilemapManager.AllTilemaps = _allTilemaps;
+        _tilemapManager.UnwalkableTilemaps = _unwalkableTilemaps;*/
+    }
+}
