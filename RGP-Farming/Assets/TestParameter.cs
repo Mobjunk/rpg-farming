e89df74d7ed59336ed7b4a39a5952cbd8dd56878
PaulDaniels <paul-daniels@hotmail.nl>
2022-06-02 16:59:55 +0200

    audio built and city

---
+++ RGP-Farming/Assets/TestParameter.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestParameter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Player _player => Player.Instance();
+    [SerializeField]FMODUnity.StudioParameterTrigger _studioTrigger;
+    [SerializeField]FMODUnity.StudioEventEmitter _eventEmitter;
+    void Start()
+    {
+        _studioTrigger = GetComponent<FMODUnity.StudioParameterTrigger>();
+        _eventEmitter = _player.GetComponent<FMODUnity.StudioEventEmitter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(_studioTrigger.Emitters.Length);
+        if(_studioTrigger.Emitters.Length == 0)
+        {
+            _studioTrigger.Emitters = new FMODUnity.EmitterRef[] {
+               // _eventEmitter.
+            };
+        }
+  //      foreach (FMODUnity.EmitterRef e in _studioTrigger.Emitters) {
+  //          if (e.Target == null || e.Target != _eventEmitter) {
+  //              e.Target = _eventEmitter;
+  //          }
+  //      }
+    }
+}
